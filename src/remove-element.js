93d29cb7eecc533cb18021465c4d7349a04f32c3
popolan1986 <popo_lan1986@163.com>
2018-12-02 14:28:48 +0800

    improve the performance

--- src/remove-element.js
+++ src/remove-element.js
@@ -23,4 +23,28 @@ var removeElement = function(nums, val) {
     return j;
 };
 
-// faster than 59.63%
+// faster than 59.63%. In this solution I didn't notice that I can change the order of the array
+
+// solution 2: change the order of the array. faster than 100%
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement2 = function(nums, val) {
+    if (nums.length === 0) {
+        return 0;
+    }
+
+    let i = 0;
+    let n = nums.length;
+    while (i < n) {
+        if (nums[i] === val) {
+            nums[i] = nums[n - 1];
+            n--;
+        } else {
+            i++;
+        }
+    }
+    return n;
+};
